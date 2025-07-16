ad4f358d54de99cb6453cc054392205fa3cbb341
Fathony Mursyid <gusfathon.fm@gmail.com>
2025-07-16 20:02:39 +0700

    executable Mumun

---
+++ run-mumun.bat
@@ -0,0 +1,44 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCqDJGmW+0g1Kw9oRAWOMya3E7gJ5tT2+ueSrAAUV+0xNobY1dQ=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJGmW+0g1Kw9oRAWOMya3E7gJ5tTW+ueSrGw0Gucnfe8=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Mumun AI 
+echo 🔁 Menjalankan Mumun AI Assistant...
+
+REM Aktifkan virtual environment
+call .venv\Scripts\activate
+
+REM Jalankan skrip utama
+python mumun.py
+
+pause
