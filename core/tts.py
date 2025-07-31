03c97cf43e925e45858c0e988cd1f383b7f3641b
Fathony Mursyid <gusfathon.fm@gmail.com>
2025-07-31 09:32:58 +0700

    Update - tweaks

--- core/tts.py
+++ core/tts.py
@@ -4,11 +4,9 @@ import pygame
 import asyncio
 from edge_tts import Communicate
 
-# Lokasi file audio TTS
 TTS_DIR = os.path.join("data", "tts")
 AUDIO_FILE = os.path.join(TTS_DIR, "audio.mp3")
 
-# Pastikan folder tts tersedia
 os.makedirs(TTS_DIR, exist_ok=True)
 
 async def speak(text: str):
